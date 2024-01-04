8502bc51535eaf203b45d7d45ae1a764a3773551
Ali Adeku <aliadeku.aam@gmail.com>
2024-01-05 00:35:31 +0100

    Add Validation to request body

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserEntity } from './user.entity';
 import { CreateUserDto } from './dto/createUser.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '@app/config';
+import { UserResponseInterface } from './types/userResponse.interface';
 @Injectable()
 export class UserService {
   constructor(
@@ -11,8 +13,41 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const checkIfEmailExists = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    const checkIfUsernameExists = await this.userRepository.findOne({
+      where: { username: createUserDto.username },
+    });
+    if (checkIfEmailExists || checkIfUsernameExists) {
+      throw new HttpException(
+        'Email or Username already exist',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      },
+      JWT_SECRET,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
